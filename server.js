4ee927c1f16183b2bf8fcf73de245f778f363f46
Konstantin <humbl3dev@gmail.com>
2019-09-20 17:50:07 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: true,
+    })
+);
+
+app.listen(PORT, () => {
+    console.log(`graphql server(port:${PORT}) running...`);
+});
