2fbdbd63e827908934db063c9f6a46f10ace2890
Konstantin M <humbl3dev@gmail.com>
2019-09-21 19:42:28 -0700

    more changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import logo from './spacex-logo.jpg';
-import Launches from './components/Launches';
-import Launch from './components/Launch';
-import './App.css';
+import React from 'react'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import logo from './spacex-logo.jpg'
+import Launches from './components/Launches'
+import Launch from './components/Launch'
+import './App.css'
 
 const client = new ApolloClient({
-  uri: '/graphql'
-});
+  uri: '/graphql',
+})
 
 function App() {
   return (
@@ -17,18 +17,22 @@ function App() {
       <Router>
         <div className="container">
           <Link to="/">
-            <img src={logo} alt="SpaceX" style={{
-              maxWidth: '200px',
-              display: 'block',
-              margin: 'auto'
-            }} />
+            <img
+              src={logo}
+              alt="SpaceX"
+              style={{
+                maxWidth: '200px',
+                display: 'block',
+                margin: 'auto',
+              }}
+            />
           </Link>
           <Route exact path="/" component={Launches}></Route>
           <Route path="/launch/:flight_number" component={Launch}></Route>
         </div>
       </Router>
     </ApolloProvider>
-  );
+  )
 }
 
-export default App;
+export default App
