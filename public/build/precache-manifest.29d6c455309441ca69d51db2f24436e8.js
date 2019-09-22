151716f1c59dda7b33001e222d413d1336b8eaab
Konstantin M <humbl3dev@gmail.com>
2019-09-21 19:49:35 -0700

    public build folder

---
+++ public/build/precache-manifest.29d6c455309441ca69d51db2f24436e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e133c8e5fd01d6c3374960c70486743c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "344ec752526b462e336a",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "9e1dc4d528ae7f9a2a66",
+    "url": "/static/js/2.596f8e50.chunk.js"
+  },
+  {
+    "revision": "344ec752526b462e336a",
+    "url": "/static/js/main.b00c2f6d.chunk.js"
+  },
+  {
+    "revision": "2055ac218fe3f7ad47ca",
+    "url": "/static/js/runtime-main.a7ca617d.js"
+  }
+]);
